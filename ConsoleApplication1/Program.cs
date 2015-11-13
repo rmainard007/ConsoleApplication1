eb8098692db75951f73f38a1cbf02fd6e9d6f05d
Ryan Mainard <rmainard007@live.com>
2015-11-12 16:40:11 -0800

    The initial upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch myStopwatch = new StopWatch();
+            bool goAgain = true;
+            string response;
+
+            while (goAgain)
+            {
+                Console.WriteLine("Selest S to start, C to Clear, and X to exit the press return. \n");
+                response = Console.ReadLine();
+
+                if ((response == "S") || (response == "s"))
+                {
+                    myStopwatch.SetStart();
+                    Console.WriteLine("Press return again to Stop the timer\n");
+                    Console.ReadKey();
+                    myStopwatch.SetStop();
+                    Console.WriteLine(myStopwatch.GetDuration().Hours + " hours, " +
+                        myStopwatch.GetDuration().Minutes + " minutes, " +
+                        myStopwatch.GetDuration().Seconds.ToString() +" seconds.\n");
+                        
+                }
+                else if (response == "C" || response == "c")
+                {
+                    myStopwatch.ClearTime();
+                }
+                else if (response == "X" || response == "x")
+                {
+                    goAgain = false;
+                }
+                else
+                {
+                    try
+                    {
+                        throw new Exception();
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("{0} is an invalid response. \n", response);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/StopWatch.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class StopWatch
+    {
+        private TimeSpan _duration;
+        private DateTime _start;
+        private DateTime _stop;
+        private bool _startProtect;
+
+        public StopWatch()
+        {
+            _duration = new TimeSpan(0, 0, 0, 0, 0);
+            _startProtect = false;
+        }
+        public void SetDuration()
+        {
+            _duration = _duration + (this.GetStop() - this.GetStart());
+        }
+        public TimeSpan GetDuration()
+        {
+            return _duration;
+        }
+        public void SetStart()
+        {
+            try
+            {
+                if (!_startProtect)
+                {
+                    _start = DateTime.Now;
+                    _startProtect = true;
+                }
+                else
+                    throw new Exception();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("You cannot Start the timer twice in a row!");
+            }
+            
+            
+        }
+        public DateTime GetStart()
+        {
+            return _start;
+        }
+        public void SetStop()
+        {
+           _stop = DateTime.Now;
+            this.SetDuration();
+            _startProtect = false;
+        }
+        public DateTime GetStop()
+        {
+            return _stop;
+        }
+        public void ClearTime()
+        {
+            _duration = new TimeSpan(0, 0, 0, 0);
+            _startProtect = false;
+        }
+
+    }
+}
